a83711e016fc9336ca6581b2645aa6804a51d81e
AntoniaObersteiner <antonia.obersteiner@gmail.com>
2025-06-30 18:18:16 +0200

    fix implementation to match theory

--- voronoi.py
+++ voronoi.py
@@ -3,10 +3,11 @@
 import turtle
 import time
 from random import random
+from math import sin, cos, pi
 
 scale = (10, 10)
 def goto(P):
-    if -10 <= P[0] <= 10 and -10 <= P[1] <= 10:
+    if -40 <= P[0] <= 40 and -40 <= P[1] <= 40:
         x = P[0] * scale[0]
         y = P[1] * scale[1]
         turtle.goto(x, y)
@@ -18,7 +19,7 @@ def dist(P, Q = (0, 0)):
     )**.5
 
 class Line:
-    def __init__(self, K, L):
+    def __init__(self, K, L, Q = None, R = None):
         """ implements line satisfying a*x + b*y = d
         with (p, 0) and (0, q) on the line, and (p, -q)
         """
@@ -50,8 +51,10 @@ class Line:
 
         self.m = (self.M[0] - self.p) / self.p # the paramter for M
 
-        self.update_t(-300)        # line is from P + t*C =: T
-        self.update_u( 300)        #           to P + u*C =: U
+        # line is from P + t*C =: T
+        self.update_t(self.where(Q) if Q is not None else -300)
+        #           to P + u*C =: U
+        self.update_u(self.where(R) if R is not None else  300)
 
     def __repr__(self, param = "t"):
         return (
@@ -64,24 +67,10 @@ class Line:
         return (self.p + t*self.p, -t*self.q)
 
     def update_t(self, t):
-        if hasattr(self, "t"):
-            print(
-                f"{self.t = :3.1f} -> {t:3.1f}, "
-                f"self.T = ({self.T[0]:3.1f}, {self.T[1]:3.1f}) -> "
-                f"({self(t)[0]:3.1f}, {self(t)[1]:3.1f}) "
-                f"= {self.__repr__('t')}"
-            )
         self.t = t
         self.T = self(t)
 
     def update_u(self, u):
-        if hasattr(self, "u"):
-            print(
-                f"{self.u = :3.1f} -> {u:3.1f}, "
-                f"self.U = ({self.U[0]:3.1f}, {self.U[1]:3.1f}) -> "
-                f"({self(u)[0]:3.1f}, {self(u)[1]:3.1f}) "
-                f"= {self.__repr__('u')}"
-            )
         self.u = u
         self.U = self(u)
 
@@ -123,14 +112,16 @@ class Line:
         I = (x, y)
         return I
 
-    def shorten(self, other, I):
+    def where(self, I):
         # where is intersection relative to line anchors
-        R =  self.relative(I)
-        S = other.relative(I)
+        R = self.relative(I)
+        r = R[0] / self.p
+        return r
 
-        # which direction is that along lines?
-        r = R[0] /  self.p
-        s = S[0] / other.p
+    def shorten(self, other, I):
+        # which direction is I along lines?
+        r =  self.where(I)
+        s = other.where(I)
 
         if (
             self .t <= r <=  self.u and
@@ -153,62 +144,77 @@ class Line:
             K[1]
         )
 
+def avg(P, Q):
+    return (
+        (P[0] + Q[0]) / 2,
+        (P[1] + Q[1]) / 2,
+    )
+def diff(P, Q):
+    return (
+        P[0] - Q[0],
+        P[1] - Q[1],
+    )
+
 def get_triple_point(P, Q, R):
     # M = (P+Q)/2
     # N = (P-Q)
     # K = (Q+R)/2
     # L = (Q-R)
+    M =  avg(P, Q)
+    N = diff(P, Q)
+    K =  avg(Q, R)
+    L = diff(Q, R)
     # (I-M)*N = 0
     # (I-K)*L = 0
     # I*N = M*N
     # I*L = K*L
     # x*Nx + y*Ny = Mx*Nx + My*Ny =: A
     # x*Lx + y*Ly = Kx*Lx + Ky*Ly =: B
+    # x(Lx-δNx) + y(Ly-δNy) = B-δA
     A = M[0]*N[0] + M[1]*N[1]
     B = K[0]*L[0] + K[1]*L[1]
-    # x(Lx-δNx) + y(Ly-δNy) = B-δA
-    δ = Lx / Nx
-    y = (B-δ*A) / (L[1]-δ*N[1])
-    x = (A-y*N[1]) / N[0]
+    if N[0] == 0:
+        y = A / N[1]
+        if L[0] == 0:
+            return None
+        x = (B-y*L[1]) / L[0]
+    else:
+        δ = L[0] / N[0]
+        divi = (L[1]-δ*N[1])
+        if divi == 0:
+            return None
+        y = (B-δ*A) / divi
+        x = (A-y*N[1]) / N[0]
     I = (x, y)
-
-    # the reduction way, just to check
-    α = Line(P, Q)
-    β = Line(P, R)
-    J = α.intersect(β)
-
-    d = dist(I, J)
-    if d > .01:
-        raise ValueError(f"wrong intersection of {P}, {Q}, {R}. {I = }, {J = }.")
 
     return I
 
-class Graph:
-    def __init__(self, points):
-        self.points = points[:]
-        self.triples = [
-            (i, j, k)
-            for i in range(len(self.points))
-            for j in range(i)
-            for k in range(k)
-        ]
-        self.triple_points = [
-            get_triple_point(
-                self.points[i],
-                self.points[j],
-                self.points[k],
-            )
-            for i, j, k in self.triples
-        ]
+def dot(P):
+    turtle.up()
+    goto(P)
+    turtle.dot()
 
-turtle.tracer(0, 0)
-def main():
+def turtle_init():
+    turtle.tracer(0, 0)
+
+def redraw(points, lines, touched = None):
+    turtle.clear()
+
+    for p, point in enumerate(points):
+        dot(point)
+        turtle.write(f"  {p}")
+
+    if touched is None:
+        for line in lines:
+            line.draw()
+    else:
+        for l in touched:
+            lines[l].draw()
+
+    turtle.update()
+
+def old_main():
     from sys import argv
-
-    def dot(P):
-        turtle.up()
-        goto(P)
-        turtle.dot()
 
     points = [
         ( 6,  2),
@@ -239,25 +245,13 @@ def main():
         line.intersect_and_shorten(Line((0.1, 0.1), (  0,  19)))
         line.intersect_and_shorten(Line((0.1, 0.1), (  0, -19)))
 
-    def redraw():
-        turtle.clear()
-
-        for p, point in enumerate(points):
-            dot(point)
-            turtle.write(f"  {p}")
-
-        for l in touched:
-            lines[l].draw()
-
-        turtle.update()
-
     touched = set()
 
     for k in range(len(lines)):
         for l in range(k):
             if len(set(pairs[k]) & set(pairs[l])) == 0:
                 continue
-            redraw()
+            redraw(points, lines, touched)
             print(f"{k} + {l}: {pairs[k]} + {pairs[l]}")
 
             if len(argv) > 1:
@@ -278,8 +272,84 @@ def main():
                     touched |= {k, l}
                     print(f"{touched = }")
 
-    redraw()
+    redraw(points, lines, touched)
     input("[ENTER] to quit")
 
+def main():
+    from sys import argv
+
+    r1 = 20
+    r2 = 30
+    speed = 1
+    points = ([
+        ( 6,  2),
+        (-4,  0),
+        (-1,  4),
+        ( 3, -2),
+    ] if len(argv) > 1 else [
+        ((random()-.5)*20, (random() - .5)*20)
+        for _ in range(20)
+    ])
+    circles = (
+        [(r1*cos(pi*i/180), r1*sin(pi*i/180)) for i in range(2, 360, 19)] +
+        [(r2*cos(pi*i/180), r2*sin(pi*i/180)) for i in range(5, 360, 19)]
+    )
+    while True:
+        points = [
+            (
+                point[0] + speed*(random()-.5),
+                point[1] + speed*(random()-.5)
+            )
+            for point in points
+        ]
+        lines = get_voronoi_lines(points + circles)
+
+        redraw(points, lines)
+    input("[ENTER] to quit")
+
+def get_voronoi_lines(points):
+    triples = [
+        (p0, p1, p2)
+        for p2 in range(len(points))
+        for p1 in range(p2)
+        for p0 in range(p1)
+    ]
+    centers = {
+        (p0, p1, p2): center
+        for p0, p1, p2 in triples
+        if (center := get_triple_point(points[p0], points[p1], points[p2])) is not None
+    }
+    distances = {
+        π: dist(center, points[π[0]])
+        for π, center in centers.items()
+    }
+    relevants = [
+        π for π in centers.keys()
+        if all(
+            distances[π] <= dist(points[p], centers[π])
+            or p in π
+            for p in range(len(points))
+        )
+    ]
+    edges = [
+        (π, ρ)
+        for π in relevants
+        for ρ in relevants
+        if len(set(π) & set(ρ)) == 2
+    ]
+
+    lines = []
+    for π, ρ in edges:
+        p, o = set(π) & set(ρ)
+
+        try:
+            lines.append(Line(points[p], points[o], centers[π], centers[ρ]))
+        except ZeroDivisionError:
+            pass
+
+    return lines
+
 if __name__ == "__main__":
+    turtle_init()
+    #old_main()
     main()
